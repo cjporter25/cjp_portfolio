13c67d15003b89e1e6949450883d38619f750df8
cjporter25 <chris.j.porter25@gmail.com>
2024-05-16 17:15:55 -0500

    Added image copyright URL's

--- src/Components/Projects/ProjectItem.tsx
+++ src/Components/Projects/ProjectItem.tsx
@@ -5,16 +5,18 @@ import "./ProjectItem.css"; // Import styling sheet
 
 interface ProjectItemProps {
   imageSrc: string;
+  imageURL: string;
   description: string;
+  
 }
 
 class ProjectItem extends React.Component<ProjectItemProps> {
   render() {
-    const { imageSrc, description } = this.props;
+    const { imageSrc, imageURL, description } = this.props;
     return (
       <div className="ProjectItem">
         <div className="ImageContainer">
-          <img src={imageSrc} alt="Project" className="ProjImg" />
+          <img src={imageSrc} alt="Project" className="ProjImg" data-src={imageURL}/>
         </div>
         <div className="DescriptionContainer">
           <div className="ProjDesc">{description}</div>
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,36 +1,43 @@
 import React from "react";
 import "./Projects.css";
 import ProjectItem from "./ProjectItem";
+
 // import squadCode from "../../Images/the_squad_code.jpg"
 // import squadImg2 from "../../Images/the_squad_champions2.jpg"
 // import squadImg3 from "../../Images/the_squad_champions3.jpg"
-// https://leagueoflegends.fandom.com/wiki/Team_Builder
+// import mpImg from "../../Images/fb_mp_logo.jpeg"
 import squadImg from "../../Images/the_squad_champions.jpg"
 import zombImg from "../../Images/zombiture_main.png"
-// import mpImg from "../../Images/fb_mp_logo.jpeg"
-// https://www.spyne.ai/blogs/used-car-dealership
 import carSales from "../../Images/used_car_salesman.jpg"
+// https://en.wikipedia.org/wiki/Python_%28programming_language%29
+//import pythonLogo from "../../Images/logo_python.png"
+// https://1000logos.net/microsoft-excel-logo/
+//import excelLogo from "../../Images/logo_excel.png"
+// https://www.facebook.com/fbmarketplace/photos/a.800314846739820/2065548730216419/?type=3
+//import fbmpLogo from "../../Images/logo_fb_mp.jpeg"
 
-
-// Sample project data (replace with actual data)
+// Project Data (replace with actual data)
 const projectsInfo = [
     {
       imageSrc: squadImg,
+      imageURL: "https://leagueoflegends.fandom.com/wiki/Team_Builder",
       // You can use "`" or "backticks", to create multi line strings
       description: "This is a placeholder description of TheSquad!",
     },
     {
       imageSrc: carSales,
+      imageURL: "https://www.spyne.ai/blogs/used-car-dealership",
       description: "This is a placeholder description of MPScrubber!",
     },
     {
       imageSrc: zombImg,
-      description: `Zombiture - In extremely early development, this game,
-      is intended to be an 8-bit styled, time-traveling, zombie ridden 2D 
-      arcade shooter. Being developed by an informal game development
-      group called HazyGames, this game will be built using the open source
-      GoDot game engine and although we're excited, we are currently unsure
-      of a release date`
+      imageURL: "N/A",
+      description: `Zombiture - In extremely early development, Zombiture's
+      goal is to provide an 8-bit styled, time-traveling, zombie ridden 2D 
+      arcade shooter experience to both PC and mobile devices. Being developed 
+      by an informal game development group called "HazyGames", this game will 
+      be built using the open source GoDot game engine and although we're excited, 
+      we are currently unsure of a release date.`
     },
   ];
 
@@ -44,6 +51,7 @@ class Projects extends React.Component{
             <ProjectItem
               key={index}
               imageSrc={project.imageSrc}
+              imageURL={project.imageURL}
               description={project.description}
             />
         ));
