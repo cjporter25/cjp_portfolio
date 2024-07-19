92b762b9769ebe1636096498d81a692aff57cb0c
cjporter25 <chris.j.porter25@gmail.com>
2024-07-19 01:11:37 -0500

    Resume Clean up

--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import "./Resume.css"; // Import styling sheet
 import resumeImage1 from '../../Documents/Basic_Porter_Resume_24_Image.jpg';
-import resumeImage2 from '../../Documents/Fancy_Porter_Resume_24_Image.jpg';
+// import resumeImage2 from '../../Documents/Fancy_Porter_Resume_24_Image.jpg';
 
 type ResumeProps = {};
 type ResumeState = {};
 class Resume extends React.Component<ResumeProps, ResumeState> {
     render() {
         const resumeURL1 = `${process.env.PUBLIC_URL}/Fancy_Porter_Resume_24.pdf`;
-        const resumeURL2 = `${process.env.PUBLIC_URL}/Basic_Porter_Resume_24.pdf`;
+        // const resumeURL2 = `${process.env.PUBLIC_URL}/Basic_Porter_Resume_24.pdf`;
         return (
             <div className="ResumeContainer">
                 <img 
@@ -17,14 +17,7 @@ class Resume extends React.Component<ResumeProps, ResumeState> {
                     src={resumeImage1}  
                 />
                 <a href={resumeURL1} download="Porter_Resume_24(Basic).pdf" className="DownloadButton">
-                    Download 'Original' Resume 
-                </a>
-                <img 
-                    className="ResumeImg" alt="Resume" 
-                    src={resumeImage2}  
-                />
-                <a href={resumeURL2} download="Porter_Resume_24(Fancy).pdf" className="DownloadButton">
-                    Download 'Fancy' Resume 
+                    Download Resume 
                 </a>
             </div>
         );
